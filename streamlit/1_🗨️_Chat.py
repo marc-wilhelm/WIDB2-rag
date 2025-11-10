1a25d65c31c8731bbac08c87d9e15e58d67ba7c9
marcwilhelm <marcwilli2015+github@gmail.com>
2025-11-10 15:00:53 +0100

    adapt streamlit

--- streamlit/1_🗨️_Chat.py
+++ streamlit/1_🗨️_Chat.py
@@ -1,19 +1,48 @@
 import streamlit as st
 
+# Creds
+creds = {
+    'apikey': 'OPENAI_API_KEY',
+    'url': ''
+}
+
+# LLM
+#llm = LangChainInterface(
+#    credentials = creds,
+#    model = '',
+#    params = {
+#        'decoding_method': 'sample',
+#        'max_new_tokens': 200,
+#        'temperature': 0.5,
+#        'top_k': 10,
+#        'top_p': 1.0,
+#    },
+#    project_id = ''
+#)
+
+
+# Generelles Layout
 st.set_page_config(
     page_title="WIDB2 - RAG Chatbot",
     page_icon="🤖"
 )
 
 st.title("RAG Chatbot")
-st.sidebar.success("Select a page above.")
+#st.sidebar.success("Select a page above.")
 
 
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = None
+# Chat
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
-my_input = st.text_input("Gebe hier deine Eingabe ein", st.session_state["my_input"])
-submit_button = st.button("Submit")
-if submit_button:
-    st.session_state["my_input"] = my_input
-    st.write("Du hast eingegeben: ", my_input)
+for message in st.session_state.messages:
+    st.chat_message(message['role']).markdown(message['content'])
+
+
+prompt = st.chat_input('Please enter your input')
+
+# Bei User Input
+if prompt:
+    st.chat_message('user').markdown(prompt)
+    st.session_state.messages.append({'role':'user', 'content':prompt})
+    #response = llm(prompt)
--- streamlit/pages/2_ℹ️_Info.py
+++ streamlit/pages/2_ℹ️_Info.py
@@ -2,4 +2,3 @@ import streamlit as st
 
 st.title("Info")
 
-st.write("Du hast eingegeben: ", st.session_state["my_input"])
